a2d9de0ef8d2df935bc2b727d9b1ea62cad12cb4
Matt Layton <mattlayt@gmail.com>
2020-05-09 23:55:07 +0100

    feat: support The Team page

---
+++ wp-content/plugins/assist-trust-team/assist-trust-team.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: Assist Trust Team
+Description: Enables custom post type "Team"
+*/
+
+add_action('init', 'at_team_cpt');
+function at_team_cpt() {
+  register_post_type('team', array(
+    'labels' => array(
+      'name' => _x('The Team'),
+      'singular_name' => _x('The Team'),
+      'add_new_item' => _x('Add Team Member'),
+      'not_found' => _x('No team members found.'),
+    ),
+    'description' => _x('The Assist Trust team'),
+    'public' => true,
+    'menu_position' => 20,
+    'supports' => array('title', 'custom-fields')
+  ));
+}
