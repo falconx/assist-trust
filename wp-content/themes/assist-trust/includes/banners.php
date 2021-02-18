38654e14d2bd1b95fef9d17b100bba58e56694fb
Matt Layton <mattlayt@gmail.com>
2021-02-18 18:28:50 +0000

    avoid additional homepage white space

--- wp-content/themes/assist-trust/includes/banners.php
+++ wp-content/themes/assist-trust/includes/banners.php
@@ -1,43 +1,41 @@
 <?php if (have_rows('banners')): ?>
-  <div>
-    <?php while (have_rows('banners')): the_row(); ?>
-      <div class="banner full-width py-3 py-sm-4 py-md-5">
-        <div class="container-xl">
-          <?php
-          
-          $banner = get_sub_field('banner');
+  <?php while (have_rows('banners')): the_row(); ?>
+    <div class="banner full-width py-3 py-sm-4 py-md-5">
+      <div class="container-xl">
+        <?php
+        
+        $banner = get_sub_field('banner');
 
-          $text = $banner['link']['link_text'];
-          $type = $banner['link']['link_type'];
+        $text = $banner['link']['link_text'];
+        $type = $banner['link']['link_type'];
 
-          $destination = ($type === 'internal')
-            ? $banner->link->destination->url
-            : $banner->link->external_destination;
-          
-          ?>
-          <div class="row mx-sm-n6 mx-lg-n4">
-            <div class="col-sm-6 px-lg-4">
-              <div class="row row-cols-1 banner--content">
-                <div class="col mt-3 mt-sm-0">
-                  <p><?php echo $banner['text']; ?></p>
-                </div>
+        $destination = ($type === 'internal')
+          ? $banner->link->destination->url
+          : $banner->link->external_destination;
+        
+        ?>
+        <div class="row mx-sm-n6 mx-lg-n4">
+          <div class="col-sm-6 px-lg-4">
+            <div class="row row-cols-1 banner--content">
+              <div class="col mt-3 mt-sm-0">
+                <p><?php echo $banner['text']; ?></p>
+              </div>
 
-                <div class="col order-first order-sm-last mt-3">
-                  <?php if (!empty($text) && !empty($destination)): ?>
-                    <a class="button button__primary" href="<?php echo $destination; ?>"<?php if ($type === 'external'): ?> target="_blank" rel="noreferrer noopener"<?php endif; ?>>
-                      <?php echo $text; ?>
-                    </a>
-                  <?php endif; ?>
-                </div>
+              <div class="col order-first order-sm-last mt-3">
+                <?php if (!empty($text) && !empty($destination)): ?>
+                  <a class="button button__primary" href="<?php echo $destination; ?>"<?php if ($type === 'external'): ?> target="_blank" rel="noreferrer noopener"<?php endif; ?>>
+                    <?php echo $text; ?>
+                  </a>
+                <?php endif; ?>
               </div>
             </div>
+          </div>
 
-            <div class="col-sm-6 px-lg-4 order-first<?php if ($banner['image_position'] == 'right'): ?> order-sm-last<?php endif; ?>">
-              <?php echo wp_get_attachment_image($banner['image'], 'banner'); ?>
-            </div>
+          <div class="col-sm-6 px-lg-4 order-first<?php if ($banner['image_position'] == 'right'): ?> order-sm-last<?php endif; ?>">
+            <?php echo wp_get_attachment_image($banner['image'], 'banner'); ?>
           </div>
         </div>
       </div>
-    <?php endwhile; ?>
-  </div>
+    </div>
+  <?php endwhile; ?>
 <?php endif; ?>
--- wp-content/themes/assist-trust/index.php
+++ wp-content/themes/assist-trust/index.php
@@ -8,6 +8,9 @@ $has_sidebar = $quote['text'] && $quote['author'];
 
 $rows = (array)get_field('content');
 
+$has_title = !is_front_page();
+$has_content = count(array_filter($rows)) || $has_title;
+
 ?>
 
 <?php get_header(); ?>
@@ -15,90 +18,50 @@ $rows = (array)get_field('content');
 <main>
   <?php include get_theme_file_path('/includes/slider.php'); ?>
 
+  <?php if (is_front_page()): ?>
+    <h1 class="visually-hidden">Assist Trust</h1>
+  <?php endif; ?>
+
   <div class="container-xl<?php echo ($has_sidebar) ? ' with-sidebar' : '' ?>">
-    <div class="row py-5">
-      <div class="col-md-<?php echo ($has_sidebar) ? '9' : '12'; ?>">
-        <?php if(is_front_page()): ?>
-          <h1 class="visually-hidden">Assist Trust</h1>
-        <?php endif; ?>
+    <?php if ($has_content || $has_sidebar): ?>
+      <div class="row py-5">
+        <div class="col-md-<?php echo ($has_sidebar) ? '9' : '12'; ?>">
+          <div class="content">
+            <?php if (!is_front_page()): ?>
+              <h1><?php the_title(); ?></h1>
+            <?php endif; ?>
 
-        <div class="content">
-          <?php if(!is_front_page()): ?>
-            <h1><?php the_title(); ?></h1>
-          <?php endif; ?>
-
-          <?php if (count(array_filter($rows))): ?>
-            <?php foreach($rows as $row): ?>
-              <div class="stack-md">
-                <?php if (isset($row['content'])): ?>
-                  <?php echo $row['content']; ?>
-                <?php elseif ($row['grid']): ?>
-                  <div class="row mx-sm-n5 grid">
-                    <?php foreach($row['grid'] as $gridItem): ?>
-                      <div class="col col-12 col-sm-6 px-sm-5">
-                        <?php echo $gridItem['content']; ?>
-                      </div>
-                    <?php endforeach; ?>
-                  </div>
-                <?php endif; ?>
-              </div>
-            <?php endforeach; ?>
-          <?php endif; ?>
-        </div>
-      </div>
-
-      <?php if ($has_sidebar): ?>
-        <section class="sidebar col-md-3 mt-3 mt-md-0" aria-labelledby="quote">
-          <h2 id="quote" class="visually-hidden">Quote</h2>
-
-          <?php get_sidebar(); ?>
-        </section>
-      <?php endif; ?>
-    </div>
-
-    <?php if (have_rows('banners')): ?>
-      <div>
-        <?php while (have_rows('banners')): the_row(); ?>
-          <div class="banner full-width py-3 py-sm-4 py-md-5">
-            <div class="container-xl">
-              <?php
-              
-              $banner = get_sub_field('banner');
-
-              $text = $banner['link']['link_text'];
-              $type = $banner['link']['link_type'];
-
-              $destination = ($type === 'internal')
-                ? $banner['link']['destination']['url']
-		: $banner['link']['external_destination'];
-
-              ?>
-              <div class="row mx-sm-n6 mx-lg-n4">
-                <div class="col-sm-6 px-lg-4">
-                  <div class="row row-cols-1 banner--content">
-                    <div class="col mt-3 mt-sm-0">
-                      <p><?php echo $banner['text']; ?></p>
+            <?php if (count(array_filter($rows))): ?>
+              <?php foreach($rows as $row): ?>
+                <div class="stack-md">
+                  <?php if (isset($row['content'])): ?>
+                    <?php echo $row['content']; ?>
+                  <?php elseif ($row['grid']): ?>
+                    <div class="row mx-sm-n5 grid">
+                      <?php foreach($row['grid'] as $gridItem): ?>
+                        <div class="col col-12 col-sm-6 px-sm-5">
+                          <?php echo $gridItem['content']; ?>
+                        </div>
+                      <?php endforeach; ?>
                     </div>
-
-                    <div class="col order-first order-sm-last mt-3">
-                      <?php if (!empty($text) && !empty($destination)): ?>
-                        <a class="button button__primary" href="<?php echo $destination; ?>"<?php if ($type === 'external'): ?> target="_blank" rel="noreferrer noopener"<?php endif; ?>>
-                          <?php echo $text; ?>
-                        </a>
-                      <?php endif; ?>
-                    </div>
-                  </div>
+                  <?php endif; ?>
                 </div>
-
-                <div class="col-sm-6 px-lg-4 order-first<?php if ($banner['image_position'] == 'right'): ?> order-sm-last<?php endif; ?>">
-                  <?php echo wp_get_attachment_image($banner['image'], 'medium'); ?>
-                </div>
-              </div>
-            </div>
+              <?php endforeach; ?>
+            <?php endif; ?>
           </div>
-        <?php endwhile; ?>
+        </div>
+
+        <?php if ($has_sidebar): ?>
+          <section class="sidebar col-md-3 mt-3 mt-md-0" aria-labelledby="quote">
+            <h2 id="quote" class="visually-hidden">Quote</h2>
+
+            <?php get_sidebar(); ?>
+          </section>
+        <?php endif; ?>
       </div>
     <?php endif; ?>
+
+    <?php include get_theme_file_path('/includes/banners.php'); ?>
   </div>
 </main>
 
